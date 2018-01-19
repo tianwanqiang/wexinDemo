cee2920c7befb51170ceac4bd1906387db469f8d
xinannan <1907853688@qq.com>
2018-01-19 23:08:28 +0800

    修改了一个字

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,8 +12,10 @@ Page({
      });
      if (this.data.time == 0) {
        wx.switchTab({
-         url: '../main/main',
+         url: '../main/main',//这是tab页面切换的方法，参数一 url
          complete: function (res) {
+         // 参数二： 一个方法，表示这个功能完成后需要做的事情
+         //你可以看微信接口文档，有详细说明，不过你先把list页面搞定，把你今天的图片换上去，然后看看效果，先把这个页面做好。
          }
        })
        clearInterval(count);
